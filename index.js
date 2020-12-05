1706e6d55b620c9d7050960da93035347448dd0a
ValentinaPanic <valentina.panic87@gmail.com>
2020-12-04 22:02:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+// console.log(saturdayFun())
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(string="*"){
+
+    return function(par="special"){
+        return `You are ${string}${par}${string}!`;
+    }
+}
+
+let Calculator = {
+
+    add: function(a,b){ return a + b},
+    subtract: function(a,b){ return a - b},
+    multiply: function(a,b){ return a * b},
+    divide: function(a,b){ return a / b},
+}
+
+function actionApplyer(int, fnArray){
+
+    for(let i = 0; i < fnArray.length; i++){
+        int = fnArray[i](int)
+    }
+    return int
+}
