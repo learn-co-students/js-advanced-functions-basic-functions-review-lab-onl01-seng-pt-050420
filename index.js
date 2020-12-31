61ecfdb69566fb4516d724231219c312bcac29e9
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-12-31 12:37:07 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+let saturdayFun = function(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = function(symbol="*") {
+    return function(adj="special") {
+        return `You are ${symbol}${adj}${symbol}!`
+    }
+}
+
+
+let Calculator = {
+    add: function() {
+        return 1+3
+    },
+    subtract: function() {
+        return 1-3
+    },
+    multiply: function() {
+        return 1*3
+    },
+    divide: function() {
+        return 10/5
+    }
+}
+
+let actionApplyer = function(num,funcs) {
+    if(funcs.length === 0) { return num}
+    funcs.forEach(func => {
+        num = func(num)
+    })
+    return num 
+}
